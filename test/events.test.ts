654c7ff74845ec8cacfc47254e7ec88955f07928
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-11-08 12:36:15 -0600

    withEventBus + withEvents error behaviour tested

--- test/events.test.ts
+++ test/events.test.ts
@@ -202,3 +202,14 @@ describe("event bus methods", () => {
     });
   });
 });
+
+describe("event bus errors", () => {
+  test("should throw error if event bus is not found", () => {
+    expect(() => withEventBus([], () => {})).toThrow("Event bus not found");
+  });
+  test("should throw error if event uuid is not found", () => {
+    expect(() => withEvents([{ event: new EventBus() }], () => {})).toThrow(
+      "Event UUID not found"
+    );
+  });
+});
